b8944763ac9826933c6728e8563821a85182e84a
Rolands Baranovskis <Baranovskis.R@gmail.com>
2021-06-01 17:18:50 +0300

    module 3 & 4

--- SEF/src/sef/module6/activity/InheritanceActivity.java
+++ SEF/src/sef/module6/activity/InheritanceActivity.java
@@ -6,15 +6,23 @@ public class InheritanceActivity {
 
 	public static void main(String[] args) {
 		
+
 		// Create Person object p. Write code to and set name of Person as Sarah Johnson
+		Person_I p = new Person_I();
 		// and age as 21
-		
-		
+		p.setAge(21);
+		p.setName("Sarah Johnson");
+
 		
 		System.out.println("-----------------------------");
 		//Create Employee object e and 
 		// set salary as 70000, title as Developer, age as 32 and name as Shawn Cun
-		
+		Employee_I e = new Employee_I();
+		e.setName("Shawn Cun");
+		e.setAge(32);
+		e.setTitle("Developer");
+		e.setSalary(70000);
+
 		
 		
 		// Print Info using Employee object
